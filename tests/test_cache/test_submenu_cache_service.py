969fa2fc225826fff9f1c575c95e060d200e21de
murekswork <murekswork@gmail.com>
2024-02-04 02:31:29 +0300

    additional tests for submenu cache service delete action

--- tests/test_cache/test_submenu_cache_service.py
+++ tests/test_cache/test_submenu_cache_service.py
@@ -35,13 +35,16 @@ class TestSubmenuCacheServiceCreate(TestSubmenuCacheService):
                                                                client: AsyncClient,
                                                                redis_client: aioredis.Redis,
                                                                clean_tables):
-        menu = await client.post(await reverse('menu-create'), json={'title': 'title', 'description': 'description'})
+        menu = await client.post(await reverse('menu-create'),
+                                 json={'title': 'title', 'description': 'description'})
         assert menu.status_code == 201
 
-        response_menu_submenus = await client.get(await reverse('submenu-read-list', target_menu_id=menu.json()['id']))
+        response_menu_submenus = await client.get(await reverse('submenu-read-list',
+                                                                target_menu_id=menu.json()['id']))
         assert response_menu_submenus.status_code == 200
 
-        response_menu_counts = await client.get(await (reverse('menu-read-counts', target_menu_id=menu.json()['id'])))
+        response_menu_counts = await client.get(await (reverse('menu-read-counts',
+                                                               target_menu_id=menu.json()['id'])))
         assert response_menu_counts.status_code == 200
 
         menu_cache = json.loads(await redis_client.get(menu.json()['id']))
@@ -53,7 +56,8 @@ class TestSubmenuCacheServiceCreate(TestSubmenuCacheService):
         assert menu_counts_cache is not None
 
         assert menu_cache
-        submenu = await client.post(await reverse('submenu-create', target_menu_id=menu.json()['id']),
+        submenu = await client.post(await reverse('submenu-create',
+                                                  target_menu_id=menu.json()['id']),
                                     json={'title': 'title', 'description': 'description'})
         assert submenu.status_code == 201
 
@@ -64,3 +68,90 @@ class TestSubmenuCacheServiceCreate(TestSubmenuCacheService):
         assert menu_cache_new is None
         assert menu_submenus_cache_new is None
         assert menu_counts_cache_new is None
+
+
+class TestSubmenuCacheServiceDelete:
+
+    @pytest.mark.asyncio
+    async def test_when_delete_submenu_then_menu_cache_deletes(self,
+                                                               client: AsyncClient,
+                                                               redis_client: aioredis.Redis,
+                                                               clean_tables):
+        menu = await client.post(await reverse('menu-create'),
+                                 json={'title': 'title', 'description': 'description'})
+        assert menu.status_code == 201
+        submenu = await client.post(await reverse('submenu-create',
+                                                  target_menu_id=menu.json()['id']),
+                                    json={'title': 'title', 'description': 'description'})
+        assert submenu.status_code == 201
+        await client.get(await reverse('menu-read', target_menu_id=menu.json()['id']))
+        assert await redis_client.get(f"{menu.json()['id']}") is not None
+        delete = await client.delete(await reverse('submenu-delete',
+                                                   target_menu_id=menu.json()['id'],
+                                                   target_submenu_id=submenu.json()['id']))
+        assert delete.status_code == 200
+        assert await redis_client.get(f'{menu.json()}') is None
+
+    @pytest.mark.asyncio
+    async def test_when_delete_submenu_then_menu_submenus_cache_deletes(self,
+                                                                        client: AsyncClient,
+                                                                        redis_client: aioredis.Redis,
+                                                                        clean_tables):
+        menu = await client.post(await reverse('menu-create'),
+                                 json={'title': 'title', 'description': 'description'})
+        assert menu.status_code == 201
+        submenu = await client.post(await reverse('submenu-create',
+                                                  target_menu_id=menu.json()['id']),
+                                    json={'title': 'title', 'description': 'description'})
+        assert submenu.status_code == 201
+        menu_submenus = await client.get(await reverse('submenu-read-list',
+                                                       target_menu_id=menu.json()['id']))
+        assert menu_submenus.status_code == 200
+        assert await redis_client.get(f"{menu.json()['id']}_submenus") is not None
+        delete = await client.delete(await reverse('submenu-delete',
+                                                   target_menu_id=menu.json()['id'],
+                                                   target_submenu_id=submenu.json()['id']))
+        assert delete.status_code == 200
+        assert await redis_client.get(f'{menu.json()["id"]}_submenus') is None
+
+    @pytest.mark.asyncio
+    async def test_when_delete_submenu_then_menus_cache_deletes(self,
+                                                                client: AsyncClient,
+                                                                redis_client: aioredis.Redis,
+                                                                clean_tables):
+        menu = await client.post(await reverse('menu-create'),
+                                 json={'title': 'title', 'description': 'description'})
+        assert menu.status_code == 201
+        submenu = await client.post(await reverse('submenu-create',
+                                                  target_menu_id=menu.json()['id']),
+                                    json={'title': 'title', 'description': 'description'})
+        assert submenu.status_code == 201
+        await client.get(await reverse('menus-read'))
+        assert await redis_client.get('menus') is not None
+        delete = await client.delete(await reverse('submenu-delete',
+                                                   target_menu_id=menu.json()['id'],
+                                                   target_submenu_id=submenu.json()['id']))
+        assert delete.status_code == 200
+        assert await redis_client.get('menus') is None
+
+    @pytest.mark.asyncio
+    async def test_when_delete_submenu_then_menu_counts_cache_deletes(self,
+                                                                      client: AsyncClient,
+                                                                      redis_client: aioredis.Redis,
+                                                                      clean_tables):
+        menu = await client.post(await reverse('menu-create'),
+                                 json={'title': 'title', 'description': 'description'})
+        assert menu.status_code == 201
+        submenu = await client.post(await reverse('submenu-create',
+                                                  target_menu_id=menu.json()['id']),
+                                    json={'title': 'title', 'description': 'description'})
+        assert submenu.status_code == 201
+        menu_counts = await client.get(await reverse('menu-read-counts',
+                                                     target_menu_id=menu.json()['id']))
+        assert menu_counts.status_code == 200
+        assert await redis_client.get(f"{menu.json()['id']}_counts") is not None
+        delete = await client.delete(await reverse('submenu-delete',
+                                                   target_submenu_id=submenu.json()['id'],
+                                                   target_menu_id=menu.json()['id']))
+        assert delete.status_code == 200
+        assert await redis_client.get(f"{menu.json()['id']}_counts") is None
