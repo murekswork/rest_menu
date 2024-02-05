4ed3b662ab43dedc1c0f4923b8af008914cf3ca5
murekswork <murekswork@gmail.com>
2024-02-05 21:56:33 +0300

    fixed double routes in docs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-from app.routing.routers import main_router
+from app.routing.dish_routes import dish_router
+from app.routing.menu_routes import menu_router
+from app.routing.submenu_routes import submenu_router
 
 app = FastAPI(title='Menu')
 
-app.include_router(main_router, prefix='/api/v1', tags=['main'])
+app.include_router(menu_router, prefix='/api/v1')
+app.include_router(submenu_router, prefix='/api/v1')
+app.include_router(dish_router, prefix='/api/v1')
