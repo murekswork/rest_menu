c64b149ba1a6ab82a47440edd2d7a5b0c27fc7eb
murekswork <murekswork@gmail.com>
2024-02-10 20:06:08 +0300

    code style fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import sys
 from typing import Any, AsyncGenerator
 
 import aioredis
@@ -15,13 +14,16 @@ import settings
 from app.db.session import get_db
 from main import app
 
-sys.path.append('..')
-
 # create async engine for interaction with database
-test_engine = create_async_engine(settings.TEST_DATABASE_URL, future=True, echo=False)
+test_engine = create_async_engine(settings.TEST_DATABASE_URL, future=True,
+                                  echo=False)
 
 # create async session for interaction with database
-test_async_session = sessionmaker(test_engine, expire_on_commit=False, class_=AsyncSession)
+test_async_session = sessionmaker(
+    test_engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
 CLEAN_TABLES = [
     'menus', 'submenus', 'dishes'
@@ -42,8 +44,16 @@ async def run_migrations():
 
 @pytest.fixture(scope='session')
 async def async_session_test():
-    engine = create_async_engine(settings.TEST_DATABASE_URL, future=True, echo=True)
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    engine = create_async_engine(
+        settings.TEST_DATABASE_URL,
+        future=True,
+        echo=True
+    )
+    async_session = sessionmaker(
+        engine,
+        expire_on_commit=False,
+        class_=AsyncSession
+    )
     yield async_session
 
 
@@ -53,7 +63,8 @@ async def clean_tables(async_session_test):
     async with async_session_test() as session:
         async with session.begin():
             for table_for_cleaning in CLEAN_TABLES:
-                await session.execute(text(f"""TRUNCATE TABLE {table_for_cleaning} CASCADE;"""))
+                await session.execute(
+                    text(f"""TRUNCATE TABLE {table_for_cleaning} CASCADE;"""))
 
 
 @pytest.fixture(scope='function')
@@ -94,13 +105,16 @@ async def client() -> AsyncGenerator[AsyncClient, Any]:
     """
 
     app.dependency_overrides[get_db] = _get_test_db
-    async with AsyncClient(app=app, base_url='http://localhost:8000') as client:
+
+    async with AsyncClient(app=app,
+                           base_url='http://localhost:8000') as client:
         yield client
 
 
 @pytest.fixture(scope='session')
 async def asyncpg_pool():
-    pool = await asyncpg.create_pool(''.join(settings.TEST_DATABASE_URL.split('+asyncpg')))
+    pool = await asyncpg.create_pool(
+        ''.join(settings.TEST_DATABASE_URL.split('+asyncpg')))
     yield pool
     pool.close()
 
@@ -110,8 +124,9 @@ async def asyncpg_pool():
 async def get_menu(client: AsyncClient):
     response = await client.get('/api/v1/menus')
     if not response.json():
-        new_menu = await client.post('/api/v1/menus', json={'title': 'New Menu',
-                                                            'description': 'New Menu Description'})
+        new_menu = await client.post('/api/v1/menus',
+                                     json={'title': 'New Menu',
+                                           'description': 'New Menu Description'})
         yield str(new_menu.json()['id'])
     else:
         yield str(response.json()[0]['id'])
@@ -122,8 +137,9 @@ async def get_menu(client: AsyncClient):
 async def get_submenu(client: AsyncClient, get_menu):
     response = await client.get(f'/api/v1/menus/{get_menu}/submenus')
     if response.json() == []:
-        new_submenu = await client.post(f'/api/v1/menus/{get_menu}/submenus', json={'title': 'New Submenu',
-                                                                                    'description': 'New Submenu'})
+        new_submenu = await client.post(f'/api/v1/menus/{get_menu}/submenus',
+                                        json={'title': 'New Submenu',
+                                              'description': 'New Submenu'})
         yield str(new_submenu.json()['id'])
     else:
         yield str(response.json()[0]['id'])
