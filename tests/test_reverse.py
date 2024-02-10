0a1a18bc41fce6e3d11df91277275c27ab11da05
murekswork <murekswork@gmail.com>
2024-02-10 20:23:10 +0300

    few tests added

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -1,11 +1,7 @@
-import sys
-
 import pytest
 from httpx import AsyncClient
 
 from tests.utils import reverse
-
-sys.path.append('..')
 
 
 class TestReverse:
@@ -14,6 +10,11 @@ class TestReverse:
     async def test_reverse_returns_correct_url(self, client: AsyncClient):
         reverse_url = await reverse('menu-create')
         assert reverse_url == '/api/v1/menus'
+
+    @pytest.mark.asyncio
+    async def test_reverse_invalid_route_raises_exception(self):
+        with pytest.raises(ValueError):
+            await reverse('invalid-route')
 
     # async def asdasd(self):
     #     response = await client.get(await reverse('menus'))
