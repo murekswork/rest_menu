57dc1f365a4808d1d7c9f87d2fde1c42f360a57d
murekswork <murekswork@gmail.com>
2024-02-10 20:20:15 +0300

    check sale method added to DishRead schema

--- app/schemas/dish_schemas.py
+++ app/schemas/dish_schemas.py
@@ -1,3 +1,4 @@
+import logging
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -8,18 +9,31 @@ from .base_schemas import TunedModel
 class DishRead(TunedModel):
     id: UUID
     title: str
-    price: float | str
     description: str
+    price: float | str
 
     def round_price(self):
         self.price = ('%.2f' % float(self.price))
         return self
 
+    async def check_sale(self, cache):
+        sale = await cache.check_sale(self.id)
+
+        v = float(self.price)
+
+        if sale is not None:
+            logging.warning(f'Sale for {self.title} is {v}')
+            v -= (v * float(sale) / 100)
+
+        self.price = ('%.2f' % v)
+        logging.info(f'Price for {self.title} is {self.price}')
+        return self
+
 
 class DishCreate(BaseModel):
     title: str
-    price: float
     description: str
+    price: float
 
 
 class DishCreateWithSubmenuId(DishCreate):
